8f8b50d028cd2c56302e859f4fb0adf3e9d9df6b
Dino <73320617+GreenDeno@users.noreply.github.com>
2024-11-21 00:07:59 -0800

    Setup database model format for species, sightings, and checklist

--- apps/birdwatching/models.py
+++ apps/birdwatching/models.py
@@ -14,10 +14,40 @@ def get_time():
     return datetime.datetime.utcnow()
 
 
-### Define your table below
-# me branch testing again and again
-# db.define_table('thing', Field('name'))
-# this is just me checking testing something with git
-## always commit your models to avoid problems later
+# Store the total different species of birds
+db.define_table('species',
+    Field('name', 'string', unique=True),
+)
+
+# Store the individual sighting events. These events contains:
+#   - the sample event identifier (shared id of the checklist)
+#   - the specie name sighted
+#   - the observation count
+#
+db.define_table('sightings',
+	Field('sample_event_identifier', 'string'),
+
+    Field('specie_name', 'string'),
+    Field('observation_count', 'integer', default=1),
+)
+
+# Store the checklist data. Each checklist contains:
+#   - the sample event identifier (unique id of the checklist)
+#   - the latitude and longitude of the observation
+#   - the observation date and time
+#   - the observer id (id of the user)
+#
+db.define_table('checklists',
+    Field('sample_event_identifier', 'string'),
+
+    Field('latitude', 'double'),
+    Field('longitude', 'double'),
+
+    Field('observation_date', 'datetime'),
+    Field('observation_time', 'time'),
+    Field('observation_duration', 'integer'),
+
+    Field('observer_id', 'string')
+)
 
 db.commit()
