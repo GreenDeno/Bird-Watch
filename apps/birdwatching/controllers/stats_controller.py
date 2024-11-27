8b30151701ceed340cf2eabdb72fae67c91cd058
Dino <73320617+GreenDeno@users.noreply.github.com>
2024-11-26 22:24:17 -0800

    Add more sample data

--- apps/birdwatching/controllers/stats_controller.py
+++ apps/birdwatching/controllers/stats_controller.py
@@ -31,66 +31,127 @@ def get_user_sightings():
         date = checklist.observation_date
 
     # return dict(sightings=sightings)
-    return [
-        {
-            "name": "Bird 1",
-            "count": 1,
-            "position": [37.7749, -122.4194],
-            "date": "2021-09-01"
-        },
-        {
-            "name": "Bird 2",
-            "count": 2,
-            "position": [39, -123],
-            "date": "2021-09-02"
-        },
-        {
-            "name": "Bird 3",
-            "count": 3,
-            "position": [40, -124],
-            "date": "2021-09-03"
-        },
-        {
-            "name": "Bird 1",
-            "count": 4,
-            "position": [40, -123],
-            "date": "2021-09-24"
-        },
-        {
-            "name": "Bird 2",
-            "count": 5,
-            "position": [39.2, -122],
-            "date": "2021-09-25"
-        },
 
-        {
-            "name": "Bird 4",
-            "count": 6,
-            "position": [38, -121],
-            "date": "2021-09-26"
-        },
-        {
-            "name": "Bird 5",
-            "count": 7,
-            "position": [37.5, -120],
-            "date": "2021-09-27"
-        },
-        {
-            "name": "Bird 6",
-            "count": 8,
-            "position": [36.8, -119],
-            "date": "2021-09-28"
-        },
-        {
-            "name": "Bird 7",
-            "count": 9,
-            "position": [36.2, -118],
-            "date": "2021-09-29"
-        },
-        {
-            "name": "Bird 8",
-            "count": 10,
-            "position": [35.9, -117],
-            "date": "2021-09-30"
-        }
-    ]
+    # TESTING DATA
+    return [
+    {
+        "name": "Eagle",
+        "count": 7,
+        "position": [35.2106, -115.4051],
+        "date": "2021-09-10"
+    },
+    {
+        "name": "Sparrow",
+        "count": 2,
+        "position": [35.7133, -123.6232],
+        "date": "2021-09-21"
+    },
+    {
+        "name": "Dove",
+        "count": 6,
+        "position": [37.7509, -120.9032],
+        "date": "2021-09-11"
+    },
+    {
+        "name": "Crow",
+        "count": 7,
+        "position": [37.4861, -116.8998],
+        "date": "2021-09-05"
+    },
+    {
+        "name": "Crow",
+        "count": 1,
+        "position": [35.4754, -122.5819],
+        "date": "2021-09-20"
+    },
+    {
+        "name": "Hawk",
+        "count": 10,
+        "position": [38.8561, -119.2971],
+        "date": "2021-09-23"
+    },
+    {
+        "name": "Pigeon",
+        "count": 10,
+        "position": [36.2837, -120.7492],
+        "date": "2021-09-03"
+    },
+    {
+        "name": "Owl",
+        "count": 9,
+        "position": [37.8627, -124.1436],
+        "date": "2021-09-03"
+    },
+    {
+        "name": "Owl",
+        "count": 9,
+        "position": [38.6476, -120.5623],
+        "date": "2021-09-17"
+    },
+    {
+        "name": "Pigeon",
+        "count": 8,
+        "position": [36.605, -122.6145],
+        "date": "2021-09-24"
+    },
+    {
+        "name": "Hawk",
+        "count": 4,
+        "position": [39.1462, -118.6397],
+        "date": "2021-09-30"
+    },
+    {
+        "name": "Pigeon",
+        "count": 6,
+        "position": [38.2927, -124.0384],
+        "date": "2021-09-28"
+    },
+    {
+        "name": "Robin",
+        "count": 2,
+        "position": [39.165, -120.8445],
+        "date": "2021-09-18"
+    },
+    {
+        "name": "Crow",
+        "count": 9,
+        "position": [37.7604, -123.384],
+        "date": "2021-09-26"
+    },
+    {
+        "name": "Crow",
+        "count": 10,
+        "position": [39.3626, -115.424],
+        "date": "2021-09-08"
+    },
+    {
+        "name": "Pigeon",
+        "count": 7,
+        "position": [37.5938, -115.9563],
+        "date": "2021-09-27"
+    },
+    {
+        "name": "Parrot",
+        "count": 1,
+        "position": [39.5291, -124.8909],
+        "date": "2021-09-28"
+    },
+    {
+        "name": "Sparrow",
+        "count": 5,
+        "position": [37.5073, -118.7656],
+        "date": "2021-09-06"
+    },
+    {
+        "name": "Sparrow",
+        "count": 4,
+        "position": [37.5346, -120.597],
+        "date": "2021-09-02"
+    },
+    {
+        "name": "Pigeon",
+        "count": 7,
+        "position": [35.786, -120.1434],
+        "date": "2021-09-07"
+    }
+]
